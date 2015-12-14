ab05fc29c638ccbdb999a87021e8d4ee28471175
Dieulin <mambouana.dieulin@sfr.fr>
2015-12-14 11:18:59 +0100

    tri par comptage en java terminé

--- src/algorithmes/TriParComptage.java
+++ src/algorithmes/TriParComptage.java
@@ -1,13 +1,78 @@
 package algorithmes;
 
 public class TriParComptage {
+/*
+fonction triParComptage(tab, borneSuperieure):
+   // Initialisation des variables
+   tabComptage[borneSuperieure + 1]
+   tailleTab = taille(tab) - 1
+   x = 0
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+   // Initialisation du tableau de comptage à 0
+   pour i = 0 à borneSuperieure:
+      tabComptage[i] = 0
+   finPour
 
+   // Création du tableau de comptage
+   pour i = 0 à tailleTab:
+      tabComptage[tab[i]]++
+   finPour
+
+   // Création du tableau trié
+   pour i = 0 à borneSuperieure:
+      pour j = 0 à tabComptage[i]:
+         tab[x++] = i
+     finPour
+  finPour
+  retourne tab 
+*/
+	private int borneSuperieure;
+	private int tailleTab;
+	private int[] tabComptage;
+	private int[] tab;
+	private int x;
+	
+	public TriParComptage(int[] tab, int borneSuperieure) {
+		
+		// Initialisation des variables
+		this.borneSuperieure = borneSuperieure;
+		this.tab = tab; 
+		this.tabComptage = new int[this.borneSuperieure+1];
+		this.tailleTab = this.tab.length;
+		this.x = 0;
 	}
-
+	
+	private void initTabComptage () {
+		for(int i=0; i<=this.borneSuperieure; i++) {
+			this.tabComptage[i] = 0;
+		}
+	}
+	
+	private void createTabComptage() {
+		for(int i=0; i<this.tailleTab; i++) {
+			this.tabComptage[this.tab[i]]++;
+		}
+	}
+	
+	private void createTabTrie() {
+		for(int i=0; i<=this.borneSuperieure; i++) {
+			for (int j = 0; j < this.tabComptage[i]; j++) {
+				this.tab[this.x++] = i;
+			}
+		}
+	}
+	
+	public int[] trier() {
+		
+		// Initialisation du tableau de comptage à 0
+		this.initTabComptage();
+		
+		// Création du tableau de comptage
+		createTabComptage();
+		
+		// Création du tableau trié
+		createTabTrie();
+		
+		return this.tab;
+	}
 }
--- src/execution/Evaluation.java
+++ src/execution/Evaluation.java
@@ -1,13 +1,77 @@
 package execution;
 
+import java.util.Arrays;
+import java.util.Random;
+
+import algorithmes.TriParComptage;
+
 public class Evaluation {
-
-	/**
-	 * @param args
-	 */
+	
+	private static int borneSuperieure;
+	private static int tab[];
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		if (args.length >0 && Integer.parseInt(args[0]) > 0) {
+			
+			// récupération nb éléments la ligne des commandes
+			int nbElements = Integer.parseInt(args[0]);
+			
+			// initialisation du tableau
+			Evaluation.initTab(nbElements);
+			Evaluation.setMax();
+			
+			// affichage tableau avant tri
+			System.out.println("Nb éléments : " + nbElements);
+			System.out.println("BorneSuperieure : " + borneSuperieure);
+			System.out.println("Tableau avant tri : " + Evaluation.affiche());
+			
+			// tri par comptage
+			TriParComptage comptage = new TriParComptage(Evaluation.tab, Evaluation.borneSuperieure);
+			comptage.trier();
+			
+			// affichage tableau après tri
+			System.out.println("Tableau après tri : " + Evaluation.affiche());
+		}
 	}
-
+	
+	/**
+	 * Initialize the array
+	 * @param nbElements nb elements
+	 */
+	private static void initTab(int nbElements) {
+		
+		Evaluation.tab = new int[nbElements];
+		int max = 100;
+		int min = 0;
+		Random rand = new Random();
+		
+		for (int i=0; i<nbElements; i++) {
+			Evaluation.tab [i] = rand.nextInt(max - min + 1) + min;
+		}
+	}
+	
+	/**
+	 * Get the array elements
+	 * @return the array elements
+	 */
+	public static String affiche() {
+		return Arrays.toString(Evaluation.tab);
+	}
+	
+	/**
+	 * Get max value of an array
+	 */
+	private static void setMax() {
+		
+		int highest = Evaluation.tab[0];
+		
+		for (int index = 1; index < Evaluation.tab.length; index ++) {
+	        if (Evaluation.tab[index] > highest) {
+	            highest = Evaluation.tab [index];
+	        }
+	    }
+		
+		Evaluation.borneSuperieure = highest;
+	}
 }
